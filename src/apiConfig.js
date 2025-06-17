d3fe1840341312614e1b3840da4bdde364961ba4
Nguyen Dinh Luu <dinhluu02@gmail.com>
2025-06-17 14:39:25 +0700

    fix bug

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,8 +1,9 @@
 // apiConfig.js
-const API_URL_BACK_END = "https://cdw-backend-vkji.onrender.com/api"; // Đặt URL API của bạn ở đây
+const API_URL_BACK_END = "https://cdw-dental-appointment-booking-backend.onrender.com"; // Đặt URL API của bạn ở đây
 
 const REACT_APP_FB_ID="1174192080256009";
 const REACT_APP_IS_LOCAL=false;
 
 export { API_URL_BACK_END, REACT_APP_FB_ID, REACT_APP_IS_LOCAL};
 
+console.log("👉 BACKEND API: ", API_URL_BACK_END);

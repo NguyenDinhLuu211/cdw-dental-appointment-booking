0b0fe93c585c5e8fa54430483d4439dffc4cbb4a
Nguyen Dinh Luu <dinhluu02@gmail.com>
2025-06-17 14:48:42 +0700

    fix bug

--- Backend/src/models/ScheduleModel.js
+++ Backend/src/models/ScheduleModel.js
@@ -1,22 +1,20 @@
 const mongoose = require('mongoose');
 
-const scheduleSchema = new mongoose.Schema(
-    {
-        
-        dayOfWeek: { type: Number, required: true },  // Ngày trong tuần (ví dụ: 0 cho Chủ nhật, 1 cho Thứ hai, ..., 6 cho Thứ bảy)
-        workingHours: [
-            {
-                doctor: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }, // Tham chiếu tới User làm bác sĩ
-                startTime: { type: String, required: true }, // Giờ bắt đầu làm việc (ví dụ: "08:00")
-                endTime: { type: String, required: true },   // Giờ kết thúc làm việc (ví dụ: "17:00")
-                isAvailable: { type: Boolean, default: true } // Trạng thái có sẵn hay không
-            }
-        ]
-    },
-    {
-        timestamps: true // Tự động thêm các trường createdAt và updatedAt
-    }
-);
+const scheduleSchema = new mongoose.Schema({
+  dayOfWeek: Number,
+  workingHours: [
+    new mongoose.Schema(
+      {
+        doctor: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+        startTime: String,
+        endTime: String,
+        isAvailable: Boolean
+      },
+      { _id: true } // ✅ Tự động thêm _id vào từng workingHour
+    )
+  ]
+});
+
 
 const Schedule = mongoose.model('Schedule', scheduleSchema);
 
